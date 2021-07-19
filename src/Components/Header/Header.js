740049420329f197626c89903c7fb32d99beb3b2
Rocio Marquesin <marquesinro@gmail.com>
2021-07-19 18:38:17 -0300

    Primer Maquetado de Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import "./Header.css"
+import "./Header.css";
+import logo from "./images/LogoHeader.svg"
+import iconFace from "./images/IconFacebook.svg";
+import iconTwi from "./images/IconTwitter.svg"
+import iconIn from "./images/IconIn.svg"
+
 
 export default function Header() {
     return (
-        <div>
-            header
-        </div>
+        <section className="containerHeader">
+            <div className="containerLogo"> <img src={logo} alt="Logo de la empresa" className="logoHeader"/></div>
+            <div >
+                <button className="buttonHeader">HOME</button>
+                <button className="buttonHeader">NOSOTROS</button>
+                <button className="buttonHeader">SOLUCIONES</button>
+                <button className="buttonHeader">BLOG</button>
+                <button className="buttonHeader">CONTACTO</button>
+             </div>
+            <div>
+            <img src={iconFace} alt="Icono de Facebook" className="iconosRedes" />
+            <img src={iconTwi} alt="Icono de Twitter" className="iconosRedes"/>
+            <img src={iconIn} alt="Icono de In" className="iconosRedes"/>
+            </div>
+        </section>
     )
 }
--- src/Container/Container.js
+++ src/Container/Container.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./Container.css";
+import Home from "../Components/Home/Home.js"
 
 
 export default function Container() {
     return (
         <div>
-            
+            <Home/>
         </div>
     )
 }
