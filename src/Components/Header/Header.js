8ea9273fe75e887c43861b0a515b3082adcfc854
Rocio Marquesin <marquesinro@gmail.com>
2021-07-20 11:14:07 -0300

    Banner

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,28 +1,31 @@
-import React from 'react';
+import React from "react";
 import "./Header.css";
-import logo from "./images/LogoHeader.svg"
+import logo from "./images/LogoHeader.svg";
 import iconFace from "./images/IconFacebook.svg";
-import iconTwi from "./images/IconTwitter.svg"
-import iconIn from "./images/IconIn.svg"
-
+import iconTwi from "./images/IconTwitter.svg";
+import iconIn from "./images/IconIn.svg";
 
 export default function Header() {
-    return (
-        <section className="Header">
-             <div className="containerHeader">
-            <div className="containerLogo"> <img src={logo} alt="Logo de la empresa" className="logoHeader"/></div>
-            <div className="containerButtonHeader">
-                <button className="buttonHeader">HOME</button>
-                <button className="buttonHeader">NOSOTROS</button>
-                <button className="buttonHeader">SOLUCIONES</button>
-                <button className="buttonHeader">BLOG</button>
-                <button className="buttonHeader">CONTACTO</button>
-             </div>
-            <div className="containerIconHeader">
-            <img src={iconFace} alt="Icono de Facebook" className="iconosRedes" />
-            <img src={iconTwi} alt="Icono de Twitter" className="iconosRedes"/>
-            <img src={iconIn} alt="Icono de In" className="iconosRedes"/>
-            </div>
-        </div></section>
-    )
+  return (
+    <section className="Header">
+      <div className="containerHeader">
+        <div className="containerLogo">
+          {" "}
+          <img src={logo} alt="Logo de la empresa" className="logoHeader" />
+        </div>
+        <div className="containerButtonHeader">
+          <button className="buttonHeader">HOME</button>
+          <button className="buttonHeader">NOSOTROS</button>
+          <button className="buttonHeader">SOLUCIONES</button>
+          <button className="buttonHeader">BLOG</button>
+          <button className="buttonHeader">CONTACTO</button>
+        </div>
+        <div className="containerIconHeader">
+          <img src={iconFace} alt="Icono de Facebook" className="iconosRedes" />
+          <img src={iconTwi} alt="Icono de Twitter" className="iconosRedes" />
+          <img src={iconIn} alt="Icono de In" className="iconosRedes" />
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -1,19 +1,23 @@
-import React from 'react'
-import "./Banner.css"
+import React from "react";
+import "./Banner.css";
 
 export default function Banner() {
-    return (
-        <div>
-           baner
-            <h1>SLOGAN PRINCIPAL DE MARCA</h1>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna ipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et d
-            </p>
-            <div>
-                <button>SOLUCIONES</button><button>NOSOTROS</button>
-            </div>
-            
-           
-            
-        </div>
-    )
+  return (
+    <div className="banner">
+      <div className="containterIntroBanner">
+        <h1 className="titleHome">
+          SLOGAN <span className="titleColor">PRINCIPAL</span> DE MARCA
+        </h1>
+        <p className="parrafoTitle">
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna ipscing elitr,
+          sed diam nonumy eirmod tempor invidunt ut labore et d
+        </p>
+        <div className="containerbuttonBanner">
+          <button>SOLUCIONES</button>
+          <button>NOSOTROS</button>
+        </div>{" "}
+      </div>
+    </div>
+  );
 }
