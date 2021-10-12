ce78621a34f259828b9ab82f7cc73d2791bf908e
marquesin <marquesinro@gmail.com>
2021-10-12 17:11:43 -0300

    Contenido de aviso de privacidad

--- src/Components/AvisoDePrivacidad/AvisoDePrivacidad.js
+++ src/Components/AvisoDePrivacidad/AvisoDePrivacidad.js
@@ -6,10 +6,183 @@ export default function AvisoDePrivacidad() {
   return (
     <section id="AvisoDePrivacidad">
       <Header />
-      <h1 className="titleTYC">
-        AVISO DE PRIVACIDAD <br />
-        OSSIPA S.A. DE C.V.
-      </h1>
+      <h1 className="titleTYC">AVISO DE PRIVACIDAD OSSIPA S.A. DE C.V.</h1>
+      <div className="containerAvisoDePrivacidad">
+        <h2>1. RESPONSABLE</h2>
+        <p>
+          En cumplimiento a la Ley Federal de Protección de Datos Personales en
+          Posesión de los Particulares, OSSIPA S.A de C.V., (en adelante OSSIPA)
+          le informa que en OSSIPA somos responsables de los datos personales
+          proporcionados a través del sitio web ossipa.com (En adelante “el
+          sitio web”). El "Usuario" podrá contactar a OSSIPA en cualquier
+          momento a través de nuestro correo electrónico contacto@ossipa.com,
+          nuestro teléfono: 55-89-36-07-36 o directamente en Cascada # 108, Col.
+          Jardines del Pedregal, Álvaro Obregón, 01900 Ciudad de México, CDMX.{" "}
+        </p>
+        <h2>2. DATOS PERSONALES</h2>
+        <p>
+          La información solicitada al usuario en “el sitio web” puede ser, de
+          manera enunciativa mas no limitativa:
+          <ul>
+            <li>Nombre completo, </li>
+            <li>Teléfono(s) de contacto</li>
+            <li>Dirección </li>
+            <li>Correo Electrónico. </li>
+          </ul>
+          Sus datos personales serán tratados con base a en los principios de
+          licitud, consentimiento, información, calidad, finalidad, lealtad,
+          proporcionalidad y responsabilidad en términos de la Legislación. Se
+          mantendrá la confidencialidad de sus datos personales estableciendo y
+          manteniendo de forma efectiva las medidas de seguridad
+          administrativas, técnicas y físicas, para evitar su daño, pérdida,
+          alteración, destrucción, uso, acceso o divulgación indebida.
+        </p>
+        <h2>3. TRANSFERENCIA DE DATOS </h2>
+        <p>
+          El titular de los datos personales, autoriza a OSSIPA S.A. de C.V., a
+          transferir los datos proporcionados por él a terceros (ya sea persona
+          física o moral), que con motivo de la relación comercial de OSSIPA
+          S.A. de C.V., así lo requieran para entero cumplimiento del negocio.
+          El titular de los datos personales, entiende y acepta que en ningún
+          momento se podrán transferir datos sensibles ni patrimoniales. En el
+          caso de no estar de acuerdo con la transmisión aquí mencionada, el
+          titular deberá ponerse en contacto al correo electrónico:
+          contacto@ossipa.com, o en nuestro teléfono: 55-89-36-07-36, en un
+          plazo no mayor a 5 (cinco) días hábiles a partir de la publicación del
+          presente aviso ya que de lo contrario y transcurrido dicho término,
+          acepta y autoriza OSSIPA S.A. de C.V., a la transferencia de los
+          mismos.
+        </p>
+        <h2>4. USO DE LA INFORMACIÓN </h2>
+        <p>
+          La información solicitada permite a OSSIPA contactar a los usuarios y
+          potenciales clientes cuando sea necesario para completar los
+          procedimientos de servicio y comerciales. Asimismo, OSSIPA utilizará
+          la información obtenida para:
+          <ul>
+            <li>
+              Proveer una mejor atención y un servicio eficiente al usuario.
+            </li>
+            <li>Informar sobre nuevos productos o servicios;</li>
+            <li>
+              Dar cumplimiento a obligaciones contraídas con nuestros clientes;
+            </li>
+            <li>Informar sobre cambios de nuestros productos o servicios</li>
+          </ul>
+          Por otra parte, informamos a usted, que sus datos personales no serán
+          compartidos con ninguna autoridad, empresa, organización o persona
+          distintas a nosotros y serán utilizados exclusivamente para los fines
+          señalados.
+        </p>
+        <h2>5. LIMITACIÓN DE USO Y DIVULGACIÓN DE INFORMACIÓN </h2>
+        <p>
+          En nuestro programa de notificación de promociones, ofertas y
+          servicios a través de correo electrónico, sólo OSSIPA tiene acceso a
+          la información recabada. Este tipo de publicidad se realiza mediante
+          avisos y mensajes promocionales de correo electrónico, los cuales solo
+          serán enviados a usted y a aquellos contactos registrados para tal
+          propósito, esta indicación podrá usted modificarla en cualquier
+          momento enviando un correo a contacto@ossipa.com. En los correos
+          electrónicos enviados, pueden incluirse ocasionalmente ofertas de
+          terceras partes que sean nuestros socios comerciales.
+        </p>
+        <h2>
+          6. DERECHOS ARCO (ACCESO, RECTIFICACIÓN, CANCELACIÓN Y OPOSICIÓN)
+        </h2>
+        <p>
+          Usted tiene en todo el momento el derecho a conocer qué datos
+          personales tenemos de usted, para qué los utilizamos y las condiciones
+          de uso que les damos (Acceso). Asimismo, es su derecho solicitar la
+          corrección de su información personal en caso de que esté
+          desactualizada, sea inexacta o incompleta (Recertificación); de igual
+          manera, tiene derecho a que su información se elimine de nuestros
+          registros o base de datos cuando considere que la misma no está siendo
+          utilizada adecuadamente (Cancelación); así como también a oponerse al
+          uso de sus datos personales para fines específicos (Oposición). Estos
+          derechos se conocen como derechos ARCO. El área responsable del manejo
+          y la administración de los datos personales es: Atención a clientes, y
+          se puede contactar a través del correo electrónico contacto@ossipa.com
+          o en nuestro teléfono 55-89-36-07-36. Nuestra dirección es: Cascada #
+          108, Col. Jardines del Pedregal, Álvaro Obregón, 01900 Ciudad de
+          México, CDMX.
+        </p>
+        <h2>7. PARA EJERCER SUS DERECHOS ARCO </h2>
+        <p>
+          Como titular de datos personales, el "Usuario" podrá ejercitar los
+          derechos ARCO (acceso, cancelación, rectificación y oposición al
+          tratamiento de sus datos personales), o bien, revocar el
+          consentimiento que haya otorgado a OSSIPA, para el tratamiento de sus
+          datos personales, enviando directamente su solicitud al responsable a
+          través de la cuenta de correo electrónico: contacto@ossipa.com. Dicha
+          solicitud deberá contener por lo menos: (a) nombre y domicilio u otro
+          medio para comunicarle la respuesta a su solicitud; (b) los documentos
+          que acrediten su identidad o, en su caso, la representación legal; (c)
+          la descripción clara y precisa de los datos personales respecto de los
+          que se solicita ejercer alguno de los derechos ARCO, (d) la
+          manifestación expresa para revocar su consentimiento al tratamiento de
+          sus datos personales y por tanto, para que no se usen; (d) cualquier
+          otro elemento que facilite la localización de los datos personales,
+          (e) firma. Su petición deberá ir acompañada de los fundamentos por los
+          que solicita dicha revocación y una identificación oficial del titular
+          de los datos o de su apoderado. En un plazo máximo de 20 (veinte) días
+          hábiles atenderemos su solicitud y le informaremos sobre la
+          procedencia de la misma a través del correo electrónico del que
+          provenga la petición. OSSIPA solicita al usuario que actualice sus
+          datos cada vez que éstos sufran alguna modificación, ya que esto
+          permitirá brindarle un servicio eficiente y personalizado.
+        </p>
+        <h2>8. COOKIES Y WEB BEACONS.</h2>
+        <p>
+          Se informa que la página www.ossipa.com maneja cookies y web beacons
+          que son estrictamente necesarios por motivos técnicos de operación, lo
+          anterior para efecto de no hacer vulnerable la seguridad de la página
+          de internet; por lo que de conformidad con el último párrafo del
+          artículo Trigésimo Primero de los Lineamientos para el Aviso de
+          Privacidad expedido por la Secretaría de Economía, dichas tecnologías
+          no pueden ser desactivadas. Si usted presume de alguna irregularidad
+          en el tratamiento de sus datos personales o existe alguna violación a
+          las disposiciones previstas en la Ley Federal de Protección de Datos
+          Personales en Posesión de los Particulares, recuerde que podrá
+          interponer la queja o denuncia correspondiente ante el Instituto
+          Nacional de Transparencia, Acceso a la Información y Protección de
+          Datos Personales (INAI). Para mayor información, puede visitar la
+          página de internet www.inai.org.mx
+        </p>
+        <h2>9. TRANSFERENCIAS DE INFORMACIÓN POR SOLICITUD JUDICIAL</h2>
+        <p>
+          OSSIPA únicamente realiza remisiones de datos para cumplir con las
+          obligaciones contraídas con los clientes. OSSIPA solo compartirá datos
+          cuando haya sido requerido por orden judicial para cumplir con las
+          disposiciones procesales.
+        </p>
+        <h2>10. CERTIFICADO DE SEGURIDAD.</h2>
+        <p>
+          En la página www.ossipa.com contamos con el servicio de candado de
+          seguridad SSL para proteger la información la información del usuario,
+          la cual se hace visible en la dirección de dominio:
+          https://www.ossipa.com/, lo cual garantiza los puntos antes
+          mencionados.
+        </p>
+        <h2>11. CAMBIOS EN EL AVISO DE PRIVACIDAD</h2>
+        <p>
+          Nos reservamos el derecho de efectuar en cualquier momento
+          modificaciones o actualizaciones al presente aviso de privacidad, para
+          la atención de novedades legislativas o jurisprudenciales, políticas
+          internas, nuevos requerimientos para la prestación u ofrecimiento de
+          nuestros servicios o productos y prácticas del mercado. Estas
+          modificaciones estarán disponibles al público a través de “el sitio
+          web”, sección Aviso de Privacidad.
+        </p>
+        <h2>12. ACEPTACIÓN DE LOS TÉRMINOS</h2>
+        <p>
+          Esta declaración de privacidad está sujeta a los términos y
+          condiciones de OSSIPA, lo cual constituye un acuerdo legal entre el
+          usuario y www.ossipa.com En el momento que el cliente/usuario utilice
+          nuestros servicios, significa que ha leído, entendido y acordado los
+          términos antes expuestos.
+        </p>
+        <p>Última actualización: Septiembre de 2021.</p>
+      </div>
     </section>
   );
 }
